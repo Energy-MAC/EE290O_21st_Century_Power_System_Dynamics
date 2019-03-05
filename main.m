e9ce9b2cd33a5049b47b4f82bb99ad2c55f9188c
Jose Daniel Lara <jdlara@berkeley.edu>
2019-03-04 18:19:40 -0800

    simplify main.m

--- main.m
+++ main.m
@@ -1,9 +1,8 @@
+clear all; clc;
 %%Load System Models and Parameters
-addpath('device_models')
+addpath(genpath('device_models'))
+addpath('utils')
 parameters
 
 %% Set- up DAE Solver 
-options = optimoptions('fsolve','Algorithm','trust-region-dogleg','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'MaxFunctionEvaluations',500000, 'MaxIterations',100000,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);
-
-%% Find Solution
-[x,fval] = fsolve(@inverter_infinite_bus,x0);%,options);
+options_dae = optimoptions('fsolve','Algorithm','trust-region-dogleg','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'MaxFunctionEvaluations',500000, 'MaxIterations',100000,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);
