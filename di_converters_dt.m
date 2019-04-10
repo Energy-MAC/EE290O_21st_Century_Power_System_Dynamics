04f5ad93d8d56aa7d45184c78b375021152842a3
keithmoffat <keithm@berkeley.edu>
2019-04-10 15:40:55 -0700

    Add files via upload

---
+++ di_converters_dt.m
@@ -0,0 +1,21 @@
+function dic_dt = di_converters_dt(v_convertersonly, ic, vdc, m, Zac_converters, inv_Lac_converters)
+%{
+Differential equations for the current on the ac side of the converters
+
+states:
+    v_convertersonly = nodal dq voltages for converter nodes only stacked 2by2by2.., size = nconverters*2
+    ic = vector of ac converter currents in dq ref frame stacked 2by2by2.., size = nconverters*2
+inputs:
+    vdc = vector of dc voltages, size = nconverters
+    m = vector of inputs stacked 2by2by2.., size = nconverters*2
+%}
+
+nconverters = length(vdc);
+vsw = zeros(nconverters*2,1);
+for i = 1:nconverters
+    vsw(2*i-1:2*i) = vdc(i)*m(2*i-1:2*i)/2; 
+end
+
+dic_dt = inv_Lac_converters*(-Zac_converters*ic + v_convertersonly - vsw);
+
+end
---
+++ dvdc_converters_dt.m
@@ -0,0 +1,21 @@
+function  dvdc_dt = dvdc_converters_dt(idc, ic, vdc, m, Gdc_converters, inv_Cdc_converters)
+%{
+Differential equations for the voltage on the dc side of the converters
+
+states:
+    idc = dc injection current, size = nconverters
+    vdc = vector of dc voltages, size = nconverters
+inputs:
+    ic = vector of ac converter currents, size = nconverters*2
+    m = vector of inputs, size + nconverters*2
+%}
+
+nconverters = length(vdc);
+isw = zeros(nconverters,1);
+for i = 1:nconverters
+    isw(i) = -ic(2*i-1:2*i)'*m(2*i-1:2*i)/2; 
+end
+
+dvdc_dt = inv_Cdc_converters*(-Gdc_converters*vdc + idc - isw);
+
+end
