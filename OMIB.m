c74459d5b3ce1e96dfe2dc1d00b39ee172b99ac2
Jose Daniel Lara <jdlara@berkeley.edu>
2019-04-16 13:03:01 -0700

    renaming

--- OMIB.m
+++ OMIB.m
@@ -6,8 +6,8 @@ parameters
 
 %% Set- up DAE Solver 
 options_dae = optimoptions('fsolve','Algorithm','trust-region-dogleg','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'MaxFunctionEvaluations',500000, 'MaxIterations',100000,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);
-x00 = fsolve(@(x)AVRSM_IF(0,x, gen_params, AVR_params, line_params, infbus_params),[1.0, 0.0, 0.0], options_dae);
+x00 = fsolve(@(x)ACGEN(0,x, gen_params, AVR_params, line_params, infbus_params),[1.0, 0.0, 0.0], options_dae);
 
 %M = eye(19);
 opts = odeset('RelTol',1e-8,'AbsTol',1e-8);
-[t,y] = ode23t(@(t,x)AVRSM_IF(t,x, gen_params, AVR_params, line_params, infbus_params), [0:0.01:4], x00', opts);
+[t,y] = ode23t(@(t,x)ACGEN(t,x, gen_params, AVR_params, line_params, infbus_params), [0:0.01:4], x00', opts);
