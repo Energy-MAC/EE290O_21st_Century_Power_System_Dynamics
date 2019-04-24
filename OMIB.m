c1865a6999bdb383a24c9a2b0cc3b915a52f74a0
Jose Daniel Lara <jdlara@berkeley.edu>
2019-04-24 15:44:41 -0700

    Add Ybus Model of 2state Machine

--- OMIB.m
+++ OMIB.m
@@ -4,72 +4,72 @@ addpath(genpath('device_models'))
 addpath('utils')
 parameters
 
-% Get Ini_cond
-Ini_V_bus = [1.0 + 1j*0;
-             1.05*cos(0.44) + 1j*1.05*sin(0.44)];
-
-X = line_params.Xl + infbus_params.Xth;
-Y = 1/(1j*X);
-
-Ybus = [Y, -Y; -Y, Y];
-
-Ini_i = Ybus*Ini_V_bus;
-
-grid_vals =   [real(Ini_V_bus(2))
-               imag(Ini_V_bus(2))];
-
-% Set- up DAE Solver 
-options_fsolve = optimoptions('fsolve','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'MaxFunctionEvaluations',500000, 'MaxIterations',100000,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);
-x00 = fsolve(@(x)ACGEN(0, x, grid_vals, machine_params, AVR_params), [0.0, 0.15, 1.1], options_fsolve);
-
+% %Machine Model only. Model 2a Classical Model
+options_fsolve = optimoptions('fsolve','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'MaxFunctionEvaluations',500000, 'MaxIterations',100000,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);           
+x00_m2 = fsolve(@(x)sync_machine_2states(0, x, [1.14, 1.05*cos(0.44), 1.05*sin(0.44)], machine_params), [1.0, 0.59], options_fsolve);           
 
 machine_params.tvar_fun = @p_ref_step;
 opts = odeset('RelTol',1e-8,'AbsTol',1e-8);
-[t,y] = ode15s(@(t,x)ACGEN(t,x, grid_vals, machine_params, AVR_params), [0:0.01:4], x00', opts);
+[t_mm2,y_mm2] = ode15s(@(t,x)sync_machine_2states(t, x, [1.14, 1.05*cos(0.44), 1.05*sin(0.44)], machine_params), [0:0.01:10], x00_m2', opts);
  
 figure(1);
-plot(t,y(:,2));
-legend({'1-bus','\infty-bus'},'Location','east')
+plot(t_mm2,y_mm2(:,2));
 %axis([0 10 0.0 0.6]);
 title('(with stepped p ref)');
 ylabel('\delta');
 xlabel('Time [s]');
 
-ini_cond_pf = [0.0 
-               0.15 
-               1.1 
-               real(Ini_V_bus(2))
-               imag(Ini_V_bus(2))];
+options_fsolve = optimoptions('fsolve','display', 'iter');           
+x00_ACGEN2 = fsolve(@(x)ACGEN2(0, x, [1.05*cos(0.44), 1.05*sin(0.44)], machine_params, AVR_params), [1.0, 0.59, 1.14], options_fsolve);           
 
-options_fsolve = optimoptions('fsolve','Algorithm','trust-region-dogleg','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'MaxFunctionEvaluations',500000, 'MaxIterations',100000,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);
-x00_ib = fsolve(@(x)ACGENIB(0, x, machine_params,  AVR_params, line_params, infbus_params), ini_cond_pf', options_fsolve);
-
-M = eye(5);
-M(4,4) = 0.0; 
-M(5,5) = 0.0;
 machine_params.tvar_fun = @p_ref_step;
-opts = odeset('RelTol',1e-8,'AbsTol',1e-8,'Mass',M);
-[t_ib,y_ib] = ode15s(@(t,x)ACGENIB(t, x, machine_params,  AVR_params, line_params, infbus_params), [0:0.01:4], x00_ib', opts);
-
+opts = odeset('RelTol',1e-8,'AbsTol',1e-8);
+[t_ACGEN2,y_ACGEN2] = ode15s(@(t,x)ACGEN2(t, x, [1.05*cos(0.44), 1.05*sin(0.44)], machine_params, AVR_params), [0:0.01:10], x00_ACGEN2', opts);
+ 
 figure(2);
-plot(t_ib,(y_ib(:,2)));
-legend({'1-bus','With AVR'},'Location','east')
+plot(t_ACGEN2,y_ACGEN2(:,2));
+%axis([0 10 0.0 0.6]);
 title('(with stepped p ref)');
 ylabel('\delta');
-xlabel('Time [s]');           
-           
-figure(3);
-plot(sqrt(y_ib(:,4).^2 + y_ib(:,5).^2))
-legend({'1-bus','\infty-bus'},'Location','east')
-title('(with stepped p ref)');
-ylabel('V_{term}');
-xlabel('Time [s]'); 
+xlabel('Time [s]');
 
+options_fsolve = optimoptions('fsolve','display', 'iter');            
+x00_OMIB2 = fsolve(@(x)ACGENIB2(0, [1.0, x, 1.05*cos(0.44), 1.05*sin(0.44)], machine_params, AVR_params, line_params, infbus_params), [0.59, 1.14], options_fsolve);           
+
+M = eye(5); M(4,4) = 0; M(5,5) = 0;
+machine_params.tvar_fun = @p_ref_step;
+opts = odeset('RelTol',1e-8,'AbsTol',1e-8, 'Mass',M);
+[t_OMIB2,y_OMIB2] = ode15s(@(t,x)ACGENIB2(t, x, machine_params, AVR_params, line_params, infbus_params), [0:0.01:10], [1.0, x00_OMIB2, 1.05*cos(0.44), 1.05*sin(0.44)]', opts);
+ 
+figure(3);
+plot(t_OMIB2,y_OMIB2(:,2));
+%axis([0 10 0.0 0.6]);
+title('(with stepped p ref)');
+ylabel('\delta');
+xlabel('Time [s]');
+
+
+
+%%
+
+%% Machine Model only. Model 2a Classical Model
+% options_fsolve = optimoptions('fsolve','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);           
+% x00_m4 = fsolve(@(x)sync_machine_4states(0, [1.0, 0.10, x], [1.14, 1.05, 0.0], machine_params), [1.0, 0.10, 1.1, 1.1], options_fsolve);           
+% 
+% machine_params.tvar_fun = @p_ref_step;
+% opts = odeset('RelTol',1e-8,'AbsTol',1e-8);
+% [t_mm4, y_mm4] = ode15s(@(t,x)sync_machine_4states(t, x, [1.0, 1.05, 0.0], machine_params), [0:0.01:10], x00_m4', opts);
+%  
+% figure(2);
+% plot(t_mm4,y_mm4(:,2));
+% title('(with stepped p ref)');
+% ylabel('\delta');
+% xlabel('Time [s]');
 
 function machine_params = p_ref_step(t,machine_params)
     if t<1
         machine_params.Pd = 0.6;
     else
-        machine_params.Pd = 0.9;
+        machine_params.Pd = 0.7;
     end
 end
--- device_models/synch_machine/AVR.m
+++ device_models/synch_machine/AVR.m
@@ -1,9 +1,9 @@
 function AVR_ODE = AVR(t, x, y, params)
         
     Emf = x(1);
-    V_td = y(1);
-    V_tq = y(2);
-    V_g = sqrt(V_td^2 + V_tq^2);
+    V_R = y(1);
+    V_I = y(2);
+    V_g = sqrt(V_R^2 + V_I^2);
     
     %get parameters
     V_sp = params.V_sp;
