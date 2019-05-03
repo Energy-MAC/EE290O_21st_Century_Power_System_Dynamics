fa9cd3bc14646edb84c080b1aac4e0031603074a
Jason MacDonald <jsmacdonald@gmail.com>
2019-05-03 10:58:41 -0700

    Updating the 2-axis model

--- SyncMachine/twoaxisSM.m
+++ SyncMachine/twoaxisSM.m
@@ -11,12 +11,12 @@ theta_s = params.theta_s;
 freq_rads = params.freq_s*2*pi(); %system reference frequency in hertz is converted to rad/s per second
 
 %parameters to be replaced by control inputs
-P_m=params.P_m;
-v_f = params.v_f;
+P_m0=params.P_m;
+v_f0 = params.v_f;
 
 % %Control Inputs:
-% v_f = cntrl.v_f; % Field Voltage
-% P_m = cntrl.v_f; % Mechanical Power (we could change this to torque, and multiply by x)
+% v_f0 = cntrl.v_f; % Field Voltage
+% P_m0 = cntrl.v_f; % Mechanical Power (we could change this to torque, and multiply by x)
 % 
 % H %Inertia constant [MVA]
 % r_a %armature resistance [pu]
@@ -34,20 +34,26 @@ v_f = params.v_f;
 %Defining the variables
 w = x(1);
 delta = x(2);
-P_e= x(3);
-i_q = x(4);
-i_d = x(5);
-% v_h = x(7);
-% p_h = x(8);
-% q_h = x(9);
+eprime_d = x(3);
+eprime_q = x(4);
+P_e= x(5);
+i_q = x(6);
+i_d = x(7);
+v_q = x(8);
+v_d = x(9);
+v_h = x(10);
+theta_h = x(11);
+p_h = x(12);
+q_h = x(13);
+v_f = x(14);
 
 % %our shortcut to defining the bus interface (voltage):
 % v_h = v_s;
 % theta_h = theta_s;
 % 
 % % voltage at bus_h (eq 15.4)
-% v_d = v_h*sin(delta - theta_h);
-% v_q = v_h*cos(delta - theta_h);
+% v_h*sin(delta - theta_h) - v_d;
+% v_h*cos(delta - theta_h) - v_q;
 % 
 % %Power injections at bus h
 % P_h = v_d*i_d+v_q*i_q;
@@ -58,8 +64,24 @@ SM_dxdt = [
     1/M *(P_m - P_e - SM_Params.D*(w-W_s)); %assumes w = 1, so w*tau_m = tau_m = P_m
     %delta dot - change in torque (eq 15.5 part 2)
     freq_rads*(w-W_s);
+    %eprime_d dot
+    (-eprime_q - (x_d - xprime_d) * i_d + v_f)/Tprime_d0;
+    %eprime_q dot
+    (-eprime_d - (x_q - xprime_q) * i_q + v_f)/Tprime_q0;
     
-    v_g*v_s/X*sin(delta - theta_s) - P_e;
+    %Algebraic Equations
+    (v_q+r_a*i_q)*i_q+(v_d+r_a*i_d)*i_d - P_e; %define P_e from the internal voltage and current
+    v_h*sin(delta - theta_h) - v_d;
+    v_h*cos(delta - theta_h) - v_q;
+    v_q + r_a*i_q - eprime_q + xprime_d * i_d;
+    v_d + r_a*i_d - eprime_d + xprime_q * i_q;
+    v_d*i_d+v_q*i_q - P_h; %Real Power Injection at bus h from internal d-q power
+    v_q*i_d-v_d*i_q - Q_h; %Reactive Power Injection at bus h from internal d-q power
+    P_m0 - P_m; %P_m0
+    v_f0-v_f;
     
+    %v_g*v_s/X*sin(delta - theta_s) - P_e;
+    
+    %Arbitrary automatic voltage regulator and 
     
     ];
