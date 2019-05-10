f4b37a0beea40c4b7b4c2265c6dfb1885faef6c5
natepallo <natepallo@gmail.com>
2019-05-10 11:23:20 -0700

    fix references in EPSR script broken from folder reorg (#19)
    * Added IRdq to DAIM
    
    Added IRdq to DAIMIB, or rather DAIMIB2 calls DAIM_IRdq for the ODEs and DAE terms. DAIM_IRdq specifically has a modified current_control_extref function that takes vg_d and vg_q inputs (y values) instead of the vg times sin or cos as in the D'Arco code.
    
    I did possibly 'revert' some DAIM and DArcoEPSR122 code because I think was following an older plan of generalization, and I just wanted it to work as a standalone for comparison.
    
    Still might not be working perfectly e.g. checking vg or vo might not be correct.. looking to explore more later.
    
    * Adds per unit conv, updated naming
    
    IR_dq --> RI_dq
    
    DAIMIB2 --> DAIMIB
    
    Refactored to use .cf and power flow initialization with per unit corrections
    
    * Create DAIMIBandRL.cf
    
    * Changes to fix EPSR122 script broken w/ refactor
    
    DAIM to DAIM_paper
    x0 to x0(1:19) - uses only inverter ics
    
    * rm DAIMIBandRL
    
    not needed in this commit
    
    * Update fig 11 to map scale w/ paper
    
    * Update time scale to match paper (0 to 4 s)

--- DArcoEPSR122.m
+++ DArcoEPSR122.m
@@ -19,8 +19,8 @@ S_BUS = V_bus.*conj(I_RI_BUS);
 %% Set- up DAE Solver 
 options_dae = optimoptions('fsolve','Algorithm','trust-region-dogleg','StepTolerance', 1e-8,'FunctionTolerance', 1e-8,'MaxFunctionEvaluations',500000, 'MaxIterations',100000,'StepTolerance',1e-8,'OptimalityTolerance', 1e-8);
 %x00_IB = fsolve(@(x)DAIMIB2(0,x,inverter_params,line_params, infbus_params),[x0,1.02,0],options_dae);
-x00_IB = fsolve(@(x)DAIMIB(0,x,inverter_params, line_params, infbus_params),[x0,V_R,V_I],options_dae);
-x00 = fsolve(@(x)DAIM(0,x,inverter_params),x0,options_dae);
+x00_IB = fsolve(@(x)DAIMIB(0,x,inverter_params, line_params, infbus_params),[x0(1:19),V_R,V_I],options_dae);
+x00 = fsolve(@(x)DAIM_paper(0,x,inverter_params),x0(1:19),options_dae);
 
 %x00(20) = 1;
 %x00(21) = 0;
@@ -36,16 +36,16 @@ opts = odeset('RelTol',1e-8,'AbsTol',1e-8);%'Mass',M);
 % script.
 inverter_params.tvar_fun = @p_ref_step;
 [t4_1_ext,y4_1_ext] = ode23t(@(t,x)DAIMIB(t,x,inverter_params, line_params, infbus_params), [0:0.01:10], x00_IB', opts_IB);
-[t4_1,y4_1] = ode23t(@(t,x)DAIM(t,x,inverter_params), [0:0.01:10], x00', opts);
+[t4_1,y4_1] = ode23t(@(t,x)DAIM_paper(t,x,inverter_params), [0:0.01:10], x00', opts);
 
 inverter_params.tvar_fun = @wg_ramp;
 [t4_2_ext,y4_2_ext] = ode23t(@(t,x)DAIMIB(t,x,inverter_params, line_params, infbus_params), [0:0.01:10], x00_IB', opts_IB);
-[t4_2,y4_2] = ode23t(@(t,x)DAIM(t,x,inverter_params), [0:0.01:10], x00', opts);
+[t4_2,y4_2] = ode23t(@(t,x)DAIM_paper(t,x,inverter_params), [0:0.01:10], x00', opts);
 
 %Figure 8: Plot power during step response. p = vod*iod+voq*ioq
 figure(8);
 plot(t4_1_ext,y4_1_ext(:,3).*y4_1_ext(:,11)+y4_1_ext(:,4).*y4_1_ext(:,12),t4_1_ext,y4_1(:,3).*y4_1(:,11)+y4_1(:,4).*y4_1(:,12));
-axis([0 10 0.4 0.8]);
+axis([0 4 0.4 0.8]);
 legend({'Y-bus','\infty-bus'},'Location','east')
 title('Power [pu] (with stepped p ref)');
 ylabel('p [pu]');
@@ -54,7 +54,7 @@ xlabel('Time [s]');
 %Figure 9: Plot w_vsm during step response.
 figure(9);
 plot(t4_1_ext,1+y4_1_ext(:,1),t4_1,1+y4_1(:,1));
-axis([0 10 .999 1.001]);
+axis([0 4 .999 1.001]);
 legend({'Y-bus','\infty-bus'},'Location','east')
 title('VSM speed (with stepped p ref)');
 ylabel('\omega_{VSM} [rad]')
@@ -64,13 +64,13 @@ xlabel('Time [s]');
 figure(10);
 subplot(2,1,1);
 plot(t4_1_ext,y4_1_ext(:,2),t4_1,y4_1(:,2));
-axis([0 10 .1 0.6]);
+axis([0 4 .1 0.6]);
 legend({'Y-bus','\infty-bus'},'Location','east')
 title('Phase angle orientation of VSM and PLL (with stepped p ref)');
 ylabel('\delta\theta_{VSM} [rad]');
 subplot(2,1,2);
 plot(t4_1_ext,y4_1_ext(:,18),t4_1,y4_1(:,18));
-axis([0 10 .05 0.2]);
+axis([0 4 .05 0.2]);
 legend({'Y-bus','\infty-bus'},'Location','east')
 ylabel('\delta\theta_{PLL} [rad]');
 xlabel('Time [s]');
@@ -79,7 +79,7 @@ xlabel('Time [s]');
 figure(11);
 %plot(t4_1,-y4_1(:,3).*y4_1(:,12)+y4_1(:,4).*y4_1(:,11));
 plot(t4_1_ext,y4_1_ext(:,19),t4_1,y4_1(:,19));
-axis([0 10 .015 .1]);
+axis([0 4 .015 .03]);
 legend({'Y-bus','\infty-bus'},'Location','east')
 title('Reactive power (with stepped p ref)');
 ylabel('q_{m} [pu]')
@@ -88,7 +88,7 @@ xlabel('Time [s]');
 %Figure 12: Plot power during freq ramp. p = vod*iod+voq*ioq
 figure(12);
 plot(t4_2_ext,y4_2_ext(:,3).*y4_2_ext(:,11)+y4_2_ext(:,4).*y4_2_ext(:,12),t4_2,y4_2(:,3).*y4_2(:,11)+y4_2(:,4).*y4_2(:,12));
-axis([0 10 0.4 0.7]);
+axis([0 4 0.4 0.7]);
 legend({'Y-bus','\infty-bus'},'Location','east')
 title('Power [pu] (with freq ramp down)');
 ylabel('p [pu]');
@@ -97,7 +97,7 @@ xlabel('Time [s]');
 %Figure 13: Plot converter current during freq ramp
 figure(13);
 plot(t4_2_ext,y4_2_ext(:,5),t4_2_ext,y4_2_ext(:,6),t4_2,y4_2(:,5),t4_2,y4_2(:,6));
-axis([0 10 -0.1 0.8]);
+axis([0 4 -0.1 0.8]);
 legend({'i_{cv,d} Y-bus','i_{cv,q} Y-bus','i_{cv,d} \infty-bus','i_{cv,q} \infty-bus'},'Location','east')
 title('Converter current (with freq ramp down)');
 ylabel('i_{cv} [pu]');
